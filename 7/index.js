6ea4f892604fe5dc4b067afc9b01b62588726bd6
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-03-15 01:31:06 -0300

    JS:menuRespOverlayOcultarOnClickOnScrollOnClickOut

---
+++ 7/index.js
@@ -0,0 +1,43 @@
+// Botón para abrir y cerrar menú (div que lo contiene)
+const barsBtn = document.querySelector(".menu-label");
+//  Menú (Hamburguesa)
+const barsMenu = document.querySelector(".navbar-list");
+//  Overlay para tirar facha abajo del menú hamburguesa y el cart.
+const overlay = document.querySelector(".overlay");
+
+// funcion para hacer ON/OFF(Click) al Menu y al overlay de fondo.
+const toggleMenu = () => {
+  barsMenu.classList.toggle("open-menu");
+  overlay.classList.toggle("show-overlay");
+};
+// funcion para cerrar el Menu responsive y remover el difuminado de fondo ,al hacer click unicamente en algun enlace
+const closeOnClick = (e) => {
+  if (!e.target.classList.contains("navbar-link")) {
+    return;
+  }
+  barsMenu.classList.remove("open-menu");
+  overlay.classList.remove("show-overlay");
+};
+
+// funcion para ocultar el Menu responsive es caso de Scrolear
+const closeMenuOnScroll = (e) => {
+  if (!barsMenu.classList.contains("open-menu")) {
+    return;
+  }
+  barsMenu.classList.remove("open-menu");
+  overlay.classList.remove("show-overlay");
+};
+
+// funcion para cerrar el menu cuando hagamos click fuera ,es decir sobre el overlay
+const closeMenuOnClickOut = (e) => {
+  barsMenu.classList.remove("open-menu");
+  overlay.classList.remove("show-overlay");
+};
+
+const init = () => {
+  barsBtn.addEventListener("click", toggleMenu);
+  barsMenu.addEventListener("click", closeOnClick);
+  window.addEventListener("scroll", closeMenuOnScroll);
+  overlay.addEventListener("click", closeMenuOnClickOut);
+};
+init();
